bc6db6ac19d466fc22d6cddf7975dcd925623390
Ramark <larin992@gmail.com>
2023-11-05 21:17:20 +0300

    task list

---
+++ task/feature_tasks/src/main/java/com/example/feature_tasks/ui/list/TaskListState.kt
@@ -0,0 +1,5 @@
+package com.example.feature_tasks.ui.list
+
+data class TaskListState(
+    val errorMessage: String? = null
+)
--- task/feature_tasks/src/main/java/com/example/feature_tasks/ui/list/TaskListViewModel.kt
+++ task/feature_tasks/src/main/java/com/example/feature_tasks/ui/list/TaskListViewModel.kt
@@ -3,12 +3,21 @@ package com.example.feature_tasks.ui.list
 import android.util.Log
 import com.example.core_common.viewModel.BaseViewModel
 import kotlinx.coroutines.Dispatchers
-import java.util.logging.Logger
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class TaskListViewModel(override val dispatchers: Dispatchers) :BaseViewModel() {
-
+    /** State экрана со списком заданий */
+    val screenState: StateFlow<TaskListState> get() = _screenState.asStateFlow()
+    private val _screenState = MutableStateFlow(TaskListState())
     override fun handleError(throwable: Throwable) {
         Log.e("TaskListViewModel", throwable.toString())
-
+        _screenState.update {
+            it.copy(
+                errorMessage = throwable.message?:"Error"
+            )
+        }
     }
 }
